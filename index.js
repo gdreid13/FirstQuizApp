027745dd4639521e5968f3e9c9fcf150eebdc3f3
gdreid13 <gdreid13@gmail.com>
2020-02-13 14:05:43 -0800

    bestowed basic functionality

--- index.js
+++ index.js
@@ -1,8 +1,156 @@
-function handleQuizApp() {
-    renderQuizAppPage();
-    handleQuestionChoices();
-    handleRightAnswer();
-    handleWrongAnswer();
-    handleEndOfGame();
-    
+function displayResult() {
+    let resultHTML = $(
+        `<div class="results">
+          <form id="js-restart-quiz">
+            <fieldset>
+              <div class="row">
+                <div class="col">
+                  <legend>Your Score is: ${STORE.score}/${STORE.questions.length}</legend>
+                </div>
+              </div>
+            
+              <div class="row">
+                <div class="col">
+                  <button type="button" id="restart"> Restart Quiz </button>
+                </div>
+              </div>
+            </fieldset>
+        </form>
+        </div>`);
+        STORE.currentQuestion = 0;
+        STORE.score = 0;
+      $("main").html(resultHTML);   
 }
+
+
+
+function questionNumber() {
+    const html = $(`<ul>
+    <li id="js-answered">Question Number: ${STORE.currentQuestion + 1}/${STORE.questions.length}</li>
+    </ul>`);
+    $(".question-number").html(html);
+}
+
+function quizScore() {
+    const html = $(`<ul>
+    <li id="js-score">Score: ${STORE.score}/${STORE.questions.length}</li>
+    </ul>`);
+    $(".quiz-score").html(html);
+}
+
+function questionOptions() {
+    let question = STORE.questions[STORE.currentQuestion];
+    for(let i = 0; i < question.options.length; i++)
+        {
+        $('.js-options').append(`
+            <input type="radio" name="options" id="option${i+1}" value="${question.options[i]}" tabindex="${i+1}"> 
+            <label for="option${i+1}"> ${question.options[i]}</label> <br/>
+            <span id="js-r${i+1}"></span>
+        `);
+        }
+}
+
+function renderQuestion() {
+    let question = STORE.questions[STORE.currentQuestion];
+    questionNumber();
+    quizScore();
+    const questionHTML = $(`
+        <div>
+            <form id="js-questions" class="question-form">
+                <fieldset>
+                    <div class="row question">
+                        <div class="col">
+                            <legend>
+                                <p>Who is this character?</p>
+                                <p title="${question.title}">
+                                <img src=${question.image} alt=${question.alt}>
+                                </p>
+                            </legend>
+                        </div>
+                    </div>
+
+                    <div class="row options">
+                        <div class="col">
+                            <div class="js-options"> </div>
+                        </div>
+                    </div>
+
+                    <div class="right-or-wrong">
+                    </div>    
+
+                    <div class="row">
+                        <div class="col">
+                            <button type = "submit" id="answer" tabindex="5">Submit</button>
+                            <button type = "button" id="next-question" tabindex="6"> Next >></button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>`);
+    $("main").html(questionHTML);
+    questionOptions();
+    $("#next-question").hide();
+}
+
+function handleQuizStart() {
+    $('#start').on('click', function(event) {
+        renderQuestion();
+    });
+}
+
+
+function handleQuestion() {
+    $('body').on('click','#next-question', function(event) {
+        if (STORE.currentQuestion === STORE.questions.length) {
+            displayResult();
+        }
+        else     {
+            renderQuestion();
+        }
+      });
+}
+
+function handleAnswer() {
+  $('body').on("submit",'#js-questions', function(event) {
+    event.preventDefault();
+    let currentQues = STORE.questions[STORE.currentQuestion];
+    let selectedOption = $("input[name=options]:checked").val();
+    if (!selectedOption) {
+      alert("Choose an option");
+      return;
+    } 
+    let id_num = currentQues.options.findIndex(i => i === selectedOption);
+    let id = "#js-r" + ++id_num;
+    $('span').removeClass("right-answer wrong-answer");
+    if(selectedOption === currentQues.answer) {
+      STORE.score++; 
+      $('.right-or-wrong').append(`Correct!<br/>`);
+      $(`${id}`).addClass("right-answer");
+    }
+    else {
+      $('.right-or-wrong').append(`Incorrect! The answer is "${currentQues.answer}"<br/>`);
+      $(`${id}`).addClass("wrong-answer");
+    }
+
+    STORE.currentQuestion++;
+    $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
+    $('#answer').hide();
+    $("input[type=radio]").attr('disabled', true);
+    $('#next-question').show();
+  });
+}
+
+function handleEndOfGame() {
+    $('body').on('click','#restart', function(event) {
+        renderQuestion();
+      });    
+}
+
+function handleQuizApp() {
+    handleQuizStart();
+    handleQuestion();
+    handleAnswer();
+    handleEndOfGame();    
+}
+
+$(handleQuizApp);
--- store.js
+++ store.js
@@ -1,14 +1,128 @@
 // This store constant holds the images for the quiz questions
 // All questions are in the form "Who is this character?", followed by one of these images
-const quizImages = [
-    'img/aragorn.jpg',
-    'img/gandalf.jpg',
-    'img/legolas.jpg',
-    'img/gimli.jpg',
-    'img/boromir.jpg',
-    'img/sauron.jpg',
-    'img/barliman.jpg',
-    'img/saruman.jpg',
-    'img/figwit.jpg',
-    'img/lobelia.jpg'
-]
+const STORE = {
+    questions: [
+        {
+            image: 'img/img1.jpg',
+            alt: "A rugged man with a sparse beard and long brown hair.",
+            title: "Wanders, but not lost.  Heir to old thrones.",
+            options: [
+                "Boromir",
+                "Aragorn",
+                "Gandalf",
+                "Barliman Butterbur"
+            ],
+            answer: "Aragorn"
+        },
+        {
+            image: 'img/img2.jpg',
+            alt: "An old man in grey with a long grey beard and a pointy hat.",
+            title:  "Booster of shorties.  Good with fireworks.",
+            options: [
+                "Boromir",
+                "Aragorn",
+                "Gandalf",
+                "Barliman Butterbur"
+            ],
+            answer: "Gandalf"
+        },
+        {
+            image: 'img/img3.jpg',
+            alt: "Blonde male with a quiver of arrows on his back.",
+            title: "Excellent shot.  Tireless.  Light on his feet.",
+            options: [
+                "Haldir",
+                "Thranduil",
+                "Legolas",
+                "Gimli"
+            ],
+            answer: "Legolas"
+        },
+        {
+            image: 'img/img4.jpg',
+            alt: "Short male with a long brown beard and an axe in each hand.",
+            title:  "Likes: Smoking, Elvish women.  Dislikes: Orcs, being tossed.",
+            options: [
+                "Haldir",
+                "Thranduil",
+                "Legolas",
+                "Gimli"
+            ],
+            answer: "Gimli"
+        },
+        {
+            image: 'img/img5.jpg',
+            alt: "Man with light brown hair and a light beard.",
+            title: "Proud warrior.  Patriot.  Vulnerable to arrows.",
+            options: [
+                "Boromir",
+                "Aragorn",
+                "Gandalf",
+                "Barliman Butterbur"
+            ],
+            answer: "Boromir"
+        },
+        {
+            image: 'img/img6.jpg',
+            alt: "Figure dressed in black with a metal helmet covering his entire head.",
+            title: "Who forges rings to further their plan for world domination?  This guy.",
+            options: [
+                "The Witch-king",
+                "Sauron",
+                "Saruman",
+                "Gollum"
+            ],
+            answer: "Sauron"
+        },
+        {
+            image: 'img/img7.jpg',
+            alt: "Male with light brown hair and mustache.",
+            title:  "Runs an Inn.  Head like a lumber-room: thing wanted, always buried.",
+            options: [
+                "Boromir",
+                "Aragorn",
+                "Gandalf",
+                "Barliman Butterbur"
+            ],
+            answer: "Barliman Butterbur"
+        },
+        {
+            image: 'img/img8.jpg',
+            alt: "Old man with long white hair and a long white beard.  Elegant staff in one hand.",
+            title:  "Bit of a shut-in.  Fancies crystal balls and rings.  Not a fan of trees.",
+            options: [
+                "Saruman",
+                "Sauron",
+                "Gandalf",
+                "Barliman Butterbur"
+            ],
+            answer: "Saruman"
+        },
+        {
+            image: 'img/img9.jpg',
+            alt: "Pale male with long brown hair.",
+            title:  "Blink and you'll miss him.  Officially cast as Elf Escort, given a name by fans.",
+            options: [
+                "Figwit",
+                "Dimwit",
+                "Datewit",
+                "Wizwit"
+            ],
+            answer: "Figwit"
+        },
+        {
+            image: 'img/img10.jpg',
+            alt: "Short woman with an odd hat and mean look on her face.",
+            title: "Another character seen only briefly.  Bilbo's nasty cousin.  Wants his hobbit-hole.",
+            options: [
+                "Elanor",
+                "Rosie",
+                "Otho",
+                "Lobelia"
+            ],
+            answer: "Lobelia"
+        },
+    ],
+    currentQuestion: 0,
+    score: 0
+}
